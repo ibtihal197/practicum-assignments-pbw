caa49cb17c642f1df5c70a7f09075ec8e60aa0d6
ibtihal197 <2010631170079@student.unsika.ac.id>
2022-04-27 01:12:52 +0700

    commit assignment CRUD

---
+++ assignment 6/kuliah.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Apr 2022 pada 20.09
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kuliah`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `krs`
+--
+
+CREATE TABLE `krs` (
+  `id` int(11) NOT NULL,
+  `mahasiswa_npm` char(13) NOT NULL,
+  `matakuliah_kodemk` char(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `krs`
+--
+
+INSERT INTO `krs` (`id`, `mahasiswa_npm`, `matakuliah_kodemk`) VALUES
+(1, '2010631170006', 'MK3'),
+(2, '2010631170023', 'MK2'),
+(3, '2010631250001', 'MK1'),
+(4, '2010631250006', 'MK4');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `npm` char(13) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jurusan` enum('Teknik Informatika','Sistem Informasi') NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`npm`, `nama`, `jurusan`, `alamat`) VALUES
+('2010631170006', 'Dea Novita', 'Teknik Informatika', 'Depok'),
+('2010631170023', 'Mulya Pratama', 'Teknik Informatika', 'Cikarang'),
+('2010631250001', 'Ahsanul Haq', 'Sistem Informasi', 'Karawang'),
+('2010631250006', 'Dias Juliansyah', 'Sistem Informasi', 'Bekasi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `matakuliah`
+--
+
+CREATE TABLE `matakuliah` (
+  `kodemk` char(6) NOT NULL,
+  `nama_mk` varchar(50) NOT NULL,
+  `jumlah_sks` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`kodemk`, `nama_mk`, `jumlah_sks`) VALUES
+('MK1', 'Basis Data', 3),
+('MK2', 'Pemrograman Berbasis Web', 3),
+('MK3', 'Algoritma dan Struktur Data', 3),
+('MK4', 'Kajian Jurnal Informatika', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `krs`
+--
+ALTER TABLE `krs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `mahasiswa_npm` (`mahasiswa_npm`),
+  ADD KEY `matakuliah_kodemk` (`matakuliah_kodemk`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`npm`);
+
+--
+-- Indeks untuk tabel `matakuliah`
+--
+ALTER TABLE `matakuliah`
+  ADD PRIMARY KEY (`kodemk`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `krs`
+--
+ALTER TABLE `krs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `krs`
+--
+ALTER TABLE `krs`
+  ADD CONSTRAINT `krs_ibfk_1` FOREIGN KEY (`mahasiswa_npm`) REFERENCES `mahasiswa` (`npm`),
+  ADD CONSTRAINT `krs_ibfk_2` FOREIGN KEY (`matakuliah_kodemk`) REFERENCES `matakuliah` (`kodemk`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
